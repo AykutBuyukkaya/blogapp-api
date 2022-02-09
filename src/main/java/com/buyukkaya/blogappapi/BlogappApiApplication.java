cee76b06573398acc71954fb44f4373252fb4453
Aykut Büyükkaya <aykutbkaya@gmail.com>
2022-02-09 22:10:48 +0300

    Initial commit.

---
+++ src/main/java/com/buyukkaya/blogappapi/BlogappApiApplication.java
@@ -0,0 +1,13 @@
+package com.buyukkaya.blogappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogappApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buyukkaya/blogappapi/BlogappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyukkaya.blogappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
